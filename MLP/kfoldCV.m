1b0947e95207030985d09706902a04ce26ae7b27
rodrigo25 <rodrigo25.25@gmail.com>
2017-06-16 16:53:32 -0300

    Correção k-fold

--- MLP/kfoldCV.m
+++ MLP/kfoldCV.m
@@ -3,14 +3,14 @@ function [ X_folds, Y_folds ] = kfoldCV( X, Y, k )
   N = size(X,1);
   
   % permutacao dos dados
-  %rp = randperm(N); % permuta os indices
-  %X = X(rp,:); % aplica permutacao em X
-  %Y = Y(rp,:); % aplica permutacao em Y
+  rp = randperm(N); % permuta os indices
+  X = X(rp,:); % aplica permutacao em X
+  Y = Y(rp,:); % aplica permutacao em Y
 
   tamFold = round(N/k); %Define a qtd de dados em cada fold
 
-  folds_X = cell(k, 1); %Cria estruturas para os folds
-  folds_Y = cell(k, 1);
+  X_folds = cell(k, 1); %Cria estruturas para os folds
+  Y_folds = cell(k, 1);
   
   for fold = 1:k 
     %Calcula intervalo de indice dos dados que formarao o fold
