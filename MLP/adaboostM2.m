e313c6f5976570f863b88f4b0f711af8d2b367d1
RenanSFreitas <renansouza92@hotmail.com>
2017-07-02 12:15:59 -0300

    Correcao (nao faz resample de D), remove suporte a mais de 1 camada oculta,

--- MLP/adaboostM2.m
+++ MLP/adaboostM2.m
@@ -1,4 +1,4 @@
-function [Yh, XYtrained] = adaboostM2(Xtr, Ytr, Xval, Yval, Xtest, Ytest,classes, T, h, nepocasMax, nnArchMode)
+function [Yh, A, B, beta, MSEtrain, MSEval] = adaboostM2(Xtr, Ytr, Xval, Yval, Xtest, Ytest,classes, T, h, nepocasMax, nnArchMode)
 % adaboostM2 pseudo-loss adaboost m2 (multi-class)
 % Xtr           - training set (N,ne)
 % Ytr           - training set labels, binary matrix (N,nc) nc = num classes
@@ -24,15 +24,15 @@ function [Yh, XYtrained] = adaboostM2(Xtr, Ytr, Xval, Yval, Xtest, Ytest,classes
     end
     % end validate input
 
-    [A, B, beta, L, XYtrained] = train(T, Xtr, Ytr, Xval, Yval, classes, h, nepocasMax, nnArchMode);
+    [A, B, beta, MSEtrain, MSEval] = train(T, Xtr, Ytr, Xval, Yval, classes, h, nepocasMax, nnArchMode);
     
     Yh = 0;
     for t=1:T
-       Yh = Yh + log(1/beta(t))*MLPsaida(Xtest, L(t), A{t}, B{t});
+       Yh = Yh + log(1/beta(t))*MLPsaida(Xtest, A{t}, B{t});
     end
 end
 
-function [A, B, beta, L, XYtrained ] = train(T, Xtr0, Ytr0, Xval, Yval, classes, h, epochs, nnArchMode)
+function [A, B, beta, MSEtrain, MSEval] = train(T, Xtr0, Ytr0, Xval, Yval, classes, h, epochs, nnArchMode)
     
     % Notacao
     % N - num de instancias
@@ -55,11 +55,9 @@ function [A, B, beta, L, XYtrained ] = train(T, Xtr0, Ytr0, Xval, Yval, classes,
     % B - hidden layer to output layer
     A = cell(T,1);
     B = cell(T,1);
-    
-    P = cell(T,1);
-    
-    XYtrained = cell(T,2);
-    
+
+    MSEtrain = zeros(T,1);
+    MSEval = zeros(T,1);
     % D - Distribution over miss-labels 
     % miss-labels = {(i,y)| i in {1;...;N} & y ~= yi }
     D = repmat(1/(N * nc-1),N,nc-1);
@@ -68,56 +66,51 @@ function [A, B, beta, L, XYtrained ] = train(T, Xtr0, Ytr0, Xval, Yval, classes,
     lastResampledRound = 1; % resample only after t > 1
     lastNNArchChangedRound = 1; % change arch only after t > 1
     
-    L = ones(T,1); %MLP hidden layers, inicializadas com 1
     t = 0; %indicates current Adaboost round
     while t<T
         t = t+1;       
         fprintf('Adaboost round %d\n', t);
         if lastResampledRound ~= t
-            [Xtr, Ytr, D] = resample(Xtr0, Ytr0, D);
+            [Xtr, Ytr] = resample(Xtr0, Ytr0, D);
             lastResampledRound = t;
         end
-        P_t = sum(D,2);
-        P_t = P_t ./ sum(P_t);
-        P{t} = P_t;
         
-        XYtrained{t,1} = Xtr;
-        XYtrained{t,2} = Ytr;
-        
-        nPerClass = sum(Ytr)
+        disp('# per class');
+        disp(sum(Ytr));
         
         % Changes MLP arch if needed
         if nnArchMode==1 && lastNNArchChangedRound~=t
-           %L(t) = L(t) + mod(t-1,2);
-           %epochs = epochs - 100;
+           % TODO mudar neuronios
+           % h = round( rand * (13 - 7) ) + 7;
            lastNNArchChangedRound=t;
         end
-        
-        V = ones(N,nc);
+                        
+        %V = ones(N,nc);
         %V(Ytr ~= 1) = D./repmat((max(D,[],2)),1,nc-1);
         
         % Trains the MLP 
-        [A_t,B_t] = MLPtreina(Xtr,Ytr,Xval,Yval,L(t),h,epochs,.01,0,0,V);
+        [A_t,B_t, netMSEtrain, netMSEval, epochsExecuted] = MLPtreina(Xtr,Ytr,Xval,Yval,h,epochs,2,1,0);
         %[A_t,B_t] = MLP_clodoaldo(Xtr,Ytr,h,epochs);
         %[A_t,B_t] = MLP_alfaAdaptativo(Xtr,Ytr,Xval,Yval,h,epochs,0);
-        
+       
         %Yh - MLPs hypothesis
-        Yh = MLPsaida(Xtr, L(t), A_t, B_t);
+        Yh = MLPsaida(Xtr0, A_t, B_t);
         
         % shows accuracy
         [~,Yhc]= max(Yh,[],2);
-        [~,Ytrc] = max(Ytr,[],2);
-        multiclassConfusionMatrix( Ytrc, Yhc, classes );
+        [~,Ytrc] = max(Ytr0,[],2);
+        acc = multiclassConfusionMatrix( Ytrc, Yhc, classes );
+        fprintf('Adaboost M2 component %d executed %d epochs with validation error %f and training acc %f\n', t, epochsExecuted, netMSEval(epochsExecuted), acc);
         
         % Calculates pseudo-loss
         
         %Yh_term - hypothesis confidence on the right labels
-        Yh_term = repmat(max(Ytr .* Yh,[],2),1,nc-1);
+        Yh_term = repmat(max(Ytr0 .* Yh,[],2),1,nc-1);
         
         %Yh_term_miss - hypothesis confidence on the miss-labels
         Yh_term_miss = zeros(N,nc-1);
         for i=1:N
-            [~,rightLabel] = max(Ytr(i,:),[],2);
+            [~,rightLabel] = max(Ytr0(i,:),[],2);
             Yh_term_miss(i,:) = [Yh(i,1:rightLabel-1) Yh(i,rightLabel+1:end)];
         end
         
@@ -149,10 +142,12 @@ function [A, B, beta, L, XYtrained ] = train(T, Xtr0, Ytr0, Xval, Yval, classes,
         A{t} = A_t;
         B{t} = B_t;
         beta(t) = beta_t;
+        MSEval(t) = netMSEval(epochsExecuted);
+        MSEtrain(t) = netMSEtrain(epochsExecuted);
     end
 end
 
-function [Xtr, Ytr, D] = resample(Xtr, Ytr, D)
+function [Xtr, Ytr] = resample(Xtr, Ytr, D)
 %Resampling based on prob distribution generated from D
     [N,~] = size(Xtr);
     P = sum(D,2);
@@ -160,5 +155,30 @@ function [Xtr, Ytr, D] = resample(Xtr, Ytr, D)
     resample = randsample(N,N,true,P);
     Xtr(:,:) = Xtr(resample,:);
     Ytr(:,:) = Ytr(resample,:);
-    D(:,:) = D(resample,:);
+end
+
+function [XtrResampled, YtrResampled] = resampleKeepClassDistr(Xtr, Ytr, D)
+%Resampling based on prob distribution generated from D keeping classes
+%distributions
+    [N,ne] = size(Xtr);
+    [~,ns] = size(Ytr);
+    nPerClass = sum(Ytr);
+    
+    XtrResampled = zeros(N,ne);
+    YtrResampled = zeros(N,ns);
+    
+    index = 1;
+    for i=1:size(Ytr,2)
+       Ytr_i = Ytr( Ytr(:,i) == 1, :);
+       Xtr_i = Xtr( Ytr(:,i) == 1, : );
+       D_i = D( Ytr(:,i) == 1, : );
+       P = sum(D_i,2);
+       P = P ./ sum(P);
+       N_i = nPerClass(i);
+       resample = randsample(N_i,N_i,true,P);
+       
+       XtrResampled(index:(index + N_i - 1),:) = Xtr_i(resample,:);
+       YtrResampled(index:(index + N_i - 1),:) = Ytr_i(resample,:);
+       index = index + N_i;
+    end
 end
