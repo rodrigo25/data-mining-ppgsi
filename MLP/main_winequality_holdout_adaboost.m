cf9f460137d87a35d5c70ade5d92a37930e4127b
RenanSFreitas <renansouza92@hotmail.com>
2017-07-02 12:16:42 -0300

    Algoritmos gulosos de seleção de features e de componentes do ensemble

--- MLP/main_winequality_holdout_adaboost.m
+++ MLP/main_winequality_holdout_adaboost.m
@@ -6,11 +6,14 @@ function [] = main_winequality_holdout_adaboost()
     load(fileName);
 
     h = 10; % MLP components hidden layer neuron count
-    nepocas = 1000; % adaboost MLP components max epoch count
+    
     classes = preProcInfo.classes;
     
-    Xtr = [Xtr;Xval];
-    Ytr = [Ytr;Yval];
+    [~,YtestClasses] = max(Ytest,[],2);
+    
+    
+    %Xtr = [Xtr;Xval];
+    %Ytr = [Ytr;Yval];
     
     %PCA
     %{
@@ -28,47 +31,99 @@ function [] = main_winequality_holdout_adaboost()
     %}
     
     %{
-    accs = zeros(size(Xtr,2),1);
+    valError = zeros(size(Xtr,2),1);
     for i=1:size(Xtr,2)
-        [ A, B ] = MLPtreina( Xtr(:,i), Ytr, Xval(:,i), Yval, 1, h, nepocas, .01, 0, 0);    
-        accs(i) = multiclassConfusionMatrix( YtestClasses, Yh, classes, 3, 'MLP' );
+        [ ~, ~, ~, MSEval, epochsExecuted ] = MLPtreina( Xtr(:,i), Ytr, Xval(:,i), Yval, 1, h, nepocas, .01, 0, 0);    
+        valError(i) = MSEval(epochsExecuted);
     end
-    [~,accsIndex] = sort(accs);
+    [~,valErrorIndex] = sort(valError);
     
-    featureCount = 1;
-    features = [accsIndex(1)]  
-    for i=2:length(accsIndex)
-        if ismember(i,features)
-           continue; 
+    features = valErrorIndex(1);
+    featuresError = valError(valErrorIndex(1));
+    for i=2:length(valErrorIndex)
+        newFeatures = [features valErrorIndex(i)];
+        [ ~, ~, ~, MSEval, epochsExecuted ] = MLPtreina( Xtr(:,newFeatures), Ytr, Xval(:,newFeatures), Yval, 1, h, nepocas, .01, 0, 0);
+        newValError = MSEval(epochsExecuted);
+        if newValError < featuresError
+           features = newFeatures; 
+           featuresError = newValError;
         end
     end       
+    
+    Xtr = Xtr(:,features);
+    Xval = Xval(:,features);
+    Xtest = Xtest(:,features);
     %}
+    %Xtr = [Xtr;Xval];
+    %Ytr = [Ytr;Yval];
     
     % MLP normal para comparar
-    [ A, B ] = MLPtreina( Xtr, Ytr, Xval, Yval, 1, h, nepocas, .01, 0, 0);
+    [ A, B ] = MLPtreina( Xtr, Ytr, Xval, Yval, h, 1000, 2, 1, 1 );
     % [A,B] = MLP_clodoaldo(Xtr,Ytr,h,nepocas);
-    %[ A, B] = MLP_alfaAdaptativo(Xtr,Ytr,Xval,Yval,h,nepocas,0);
+    %[ A, B] = MLP_alfaAdaptativo(Xtr,Ytr,Xval,Yval,h,1000,0);
     
-    Yh = MLPsaida( Xtest, 1, A, B );
+    Yh = MLPsaida( Xtest, A, B );
     [~,Yh] = max(Yh,[],2);
-    fprintf('MLP answer (%d hidden layer neurons, %d epochs)\n', h, nepocas);
-    [~,YtestClasses] = max(Ytest,[],2);
-    mlpAcc = multiclassConfusionMatrix( YtestClasses, Yh, classes, 3, 'MLP' );
+    fprintf('MLP answer (%d hidden layer neurons, %d epochs)\n', h, 1000);
+    
+    mlpAcc = multiclassConfusionMatrix( YtestClasses, Yh, classes, 2, 'MLP' );
     fprintf('MLP accuracy %f\n', mlpAcc);
-    %pause;
+    %return;
     T = 100; % adaboost rounds
      
     %K = preProcInfo.k;
 
-    [Yh, ~] = adaboostM2(Xtr, Ytr, Xval, Yval, Xtest, Ytest, classes, T, h, nepocas, 1);
+    [Yh, Aen, Ben, beta, MSEtrain, MSEval] = adaboostM2(Xtr, Ytr, Xval, Yval, Xtest, Ytest, classes, T, h, 2000, 1);
 
-    fprintf('Adaboost global answer (%d components, %d hidden layer neurons, %d epochs)\n', T, h, nepocas);
+    fprintf('Adaboost global answer (%d components, %d hidden layer neurons, %d epochs)\n', T, h, 2000);
     [~,Yh]= max(Yh,[],2);
-    [~,Ytest] = max(Ytest,[],2);
 
-    ensembleAcc = multiclassConfusionMatrix( Ytest, Yh, classes, 3, 'Adaboost' );
+    ensembleAcc = multiclassConfusionMatrix( YtestClasses, Yh, classes, 3, 'Adaboost' );
+
+    [~, sortedComponents] = sort(MSEval);
+    
+    bestComponents = sortedComponents(1);
+    bestMSE = MSEval(sortedComponents(1));
+    bestBetas = beta(sortedComponents(1));
+    for t=2:T
+        newComponents = [bestComponents sortedComponents(t)];
+        newBetas = [bestBetas beta(sortedComponents(t))];
+        %newBetas = (newBetas * sum(beta)) ./ sum(newBetas);
+        
+        Yh = 0;
+        for i=1:length(newComponents)
+            componentIndex = newComponents(i);
+            Ac = Aen{componentIndex};
+            Bc = Ben{componentIndex};
+            betac = newBetas(i);
+            Yh = Yh + log(1/betac)*MLPsaida(Xval, Ac, Bc);
+        end
+        newErr = Yh-Yval;
+        newMse = sum(sum( newErr.^2 ))/size(Xval,1);
+        
+        if bestMSE > newMse
+           bestMSE = newMse;
+           bestComponents = newComponents;
+           bestBetas = newBetas;
+        end
+    end
+    
+    Yh = 0;
+    for i=1:length(bestComponents)
+        componentIndex = bestComponents(i);
+        Ac = Aen{componentIndex};
+        Bc = Ben{componentIndex};
+        betac = beta(componentIndex);
+        Yh = Yh + log(1/betac)*MLPsaida(Xtest, Ac, Bc);
+    end
+    [~,Yh]= max(Yh,[],2);
+    selectedEnsembleAcc = multiclassConfusionMatrix( YtestClasses, Yh, classes, 2, 'MLP' );
+    
     fprintf('MLP accuracy %f\n', mlpAcc);
     fprintf('Ensemble accuracy %f\n', ensembleAcc);
+    fprintf('Ensemble accuracy (selected components) %f\n', selectedEnsembleAcc);
+    fprintf('Ensemble #components (selected components) %d\n', length(bestComponents));
+
     %{
     for t=1:T
        Xtrained = XYtrained{t,1}; 
