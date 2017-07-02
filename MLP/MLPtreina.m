5d6a2dcf7d0cac1d5624d70be35162b907c425d8
RenanSFreitas <renansouza92@hotmail.com>
2017-07-02 12:17:16 -0300

    Bisseção e gradiente conjugado PR (primeira versao)

--- MLP/MLPtreina.m
+++ MLP/MLPtreina.m
@@ -1,4 +1,4 @@
-function [A, B, MSEtrain, MSEval, epochsExecuted] = MLPtreina(X, Yd, Xval, Ydval, L, h, mE, alfa0, alfaDecay, earlyStopping, V)
+function [A, B, MSEtrain, MSEval, epochsExecuted] = MLPtreina(X, Yd, Xval, Ydval, h, mE, alfaType, gradientType, earlyStopping, alfa0)
 % MLPtreina treina uma MLP com 1 camada oculta para um dataset de N linhas,
 % ne colunas e nc classes.
 % X         - Training dataset, matriz (N,ne)
@@ -9,101 +9,56 @@ function [A, B, MSEtrain, MSEval, epochsExecuted] = MLPtreina(X, Yd, Xval, Ydval
 % h         - Nº neuronios em cada camada oculta
 % mE        - max iterations
 % alfa0     - Taxa de aprendizado inicial
-% alfaDecay     -
+% alfaType     -
 %                   0: constante, sem decaimento
 %                   1: decaimento pela metade step (a cada 5 epocas)
-%                   2: dinamica, cai 10% se o erro do dataset de validacao
-%                   sobe e sobe 10% caso contrario
+%                   2: bisseção
+% gradientType -
+%                   0: simples
+%                   1: conjugado
 % earlyStopping -
 %                   0: nao para o treino se o MSE de validacao subir
 %                   1: para o treino se o MSE de validacao subir
-% V         - matriz (N,nc) de pesos para os erros, opcional
-    
-    [N, ne] = size(X);
-    ns = size(Yd,2);
-    
-    if nargin < 11
-        V = ones(N,ns);
-        if nargin < 10
-           earlyStopping = 0; 
+    if nargin < 9
+        if alfaType ~= 2
+            error('Alfa must be defined if when bisection method is not used');
         end
     end
     
-    X = [ones(N,1),X]; % add bias
-
-    A = cell(L,1);
-    A{1} = rand(h, ne+1);
-    for i=2:L
-       A{i} = rand(h/(2^(i-1)), h+1);
-    end
-    B = rand(ns, (h/(2^(L-1)))+1); 
-
-    maxMSE = 1e-5;
+    [N, ne] = size(X);
+    [~, ns] = size(Yd);
+    
+    %Inicializa
+    A = rand(h, ne+1)/10;
+    B = rand(ns, h+1)/10; 
+    [Y, ~] = MLPsaida( X, A, B );
+	%Erro inicial
+    err = Y-Yd;
+    mse = sum(sum( err.^2 ))/N;
 
     MSEtrain = [];
     MSEval = [];
-    alfa = alfa0;
+    alfas = [];
     minAlfa = .001;
-    maxAlfa = .1;
+    
     epochsExecuted = 0;
-
-    Zin = cell(L,1);
-    Z = cell(L,1);
-    gradA = cell(L,1);
-    deltaA = cell(L,1);
-    
-    alfas = [alfa];
-    
-    for it=1:mE
-      
-      epochsExecuted = it;
+    it = 0;
+    while it < mE && mse > 1e-5
+      it = it + 1;
       
       %Feedfoward
-      Zin{1} = X*A{1}';
-      Z{1} =  1./(1+exp(-Zin{1}));
-      for i=2:L
-         Zin{i} = [ones(size(Z{i-1},1),1) Z{i-1}] * A{i}';
-         Z{i} = 1./(1+exp(-Zin{i}));
-      end
-      Yin = [ones(N,1),Z{L}]*B';
-      Y = 1./(1+exp(-Yin));
+      [Y, Z] = MLPsaida( X, A, B );
 
       %Training error
-      err = (Y-Yd);
+      err = Y-Yd;
       mse = sum(sum( err.^2 ))/N;
     
       %Validation error
-      Yval = MLPsaida(Xval,L,A,B);
-      errVal = (Yval-Ydval);
+      Yval = MLPsaida(Xval, A, B);
+      errVal = Yval-Ydval;
       mseVal = sum(sum( errVal.^2 ))/size(Xval,1);
      
-      %Backpropagation
-      gradB = (V.*err).*(1-Y).*Y;
-      g = gradB;
-      G = B;
-      for i=L:-1:1
-         gradA{i} = (g*G(:,2:end)).*(Z{i}.*(1-Z{i})); 
-         if i > 1
-             Gin = [ones(size(Z{i-1},1),1), Z{i-1}];
-         else
-             Gin = X;
-         end
-         deltaA{i} = gradA{i}' * Gin;
-         g = gradA{i};
-         G = A{i};
-      end
-
-      deltaB = gradB' * [ones(N,1),Z{L}];
-
-      for i=1:L
-          A{i} = A{i} - alfa * deltaA{i};
-      end
-      B = B - alfa * deltaB;
-      
-      if mse<maxMSE
-          break;
-      end
-      
+      %Saves A, B to restore (early stopping case)
       if mod(it,20) == 0
          savedA = A;
          savedB = B;
@@ -111,7 +66,7 @@ function [A, B, MSEtrain, MSEval, epochsExecuted] = MLPtreina(X, Yd, Xval, Ydval
       
       % if early stopping is enabled
       % and if the last 20 epochs show no improvement
-      if earlyStopping && it > 1 && mod(it-1,20) == 0 && mseVal >= MSEval(it-1)
+      if earlyStopping && it > 1 && mod(it-1,20) == 0 && mseVal > MSEval(it-1)
           % check last epochs improvements
           % assume no improvement
           lastEpochsNoImprovement = 1;
@@ -131,53 +86,96 @@ function [A, B, MSEtrain, MSEval, epochsExecuted] = MLPtreina(X, Yd, Xval, Ydval
           end
       end
       
-      %Learning rate update
-      if alfaDecay == 0
-          alfa = alfa0;
-      elseif alfaDecay == 1 && mod(it,5) == 0 && alfa > minAlfa
-          alfa = .5 * alfa;
-      elseif alfaDecay == 2
-          if it==1
-             mseAux = +Inf;
-          end
-          if mseAux > mse
-              if alfa > .005
-                alfa = alfa*.9;
+      %Backpropagation
+      gradB = 1/N * err.*((1-Y).*Y);
+      gradA = 1/N * (gradB*B(:,2:end)).*(Z.*(1-Z));
+     
+      if gradientType == 1
+          % gradiente conjugado polak-ribiere
+          if it == 1
+              dB = gradB;
+              dA = gradA;
+
+              gradB_old = gradB;
+              gradA_old = gradA;
+
+              normA = zeros(h,1);
+              for i=1:h
+                 normA(i) = gradA(:,i)' * gradA(:,i);
               end
-          else
-             alfa = alfa*1.1; 
-          end
-          mseAux = mse;
-          %{
-          Aaux = A;
-          Baux = B;
-          while mseAux > mse
-              alfa = alfa * 0.9;
-              Aaux = cell(L,1);
-              for l=1:L
-                  Aaux{l} = A{l} - alfa * deltaA{l};
+              normB = zeros(ns,1);
+              for i=1:ns
+                 normB(i) = gradB(:,i)' * gradB(:,i);
               end
-              Baux = B - alfa * deltaB;
-              Y = MLPsaida( X(:,2:end), L, Aaux, Baux );
-              err = (Y-Yd);
-              mseAux = sum(sum( err.^2 ))/N;
-          end
-          alfa = alfa/0.9;
-          A = Aaux;
-          B = Baux;
-          
-          mseAux2 = mse;
-          mse = mseAux;
-          mseAux = mseAux2;
-          %}
+              
+              gB = sqrt(normB');
+              gA = sqrt(normA');
+              gradB = dB ./ repmat(gB,N,1);
+              gradA = dA ./ repmat(gA,N,1);
+          else              
+              newNormA = zeros(h,1);
+              for i=1:h
+                 newNormA(i) = gradA(:,i)' * gradA(:,i);
+              end
+              newNormB = zeros(ns,1);
+              for i=1:ns
+                 newNormB(i) = gradB(:,i)' * gradB(:,i);
+              end
+              
+              if mod(it,ns) ~= 0
+                  beta = ((gradB - gradB_old)'*gradB)./repmat(gB,ns,1);
+                  dB = gradB + dB * beta;
+              else
+                  dB = gradB;
+              end
+                            
+              if mod(it,h) ~= 0
+                  beta = ((gradA - gradA_old)'*gradA)./repmat(gA,h,1);
+                  dA = gradA + dA * beta;
+              else
+                  dA = gradA;
+              end
+
+              normB = newNormB;
+              normA = newNormA;
+                            
+              gradB_old = gradB;
+              gradA_old = gradA;
+              
+              gB = sqrt(normB');
+              gA = sqrt(normA');
+              gradB = dB ./ repmat(gB,N,1);
+              gradA = dA ./ repmat(gA,N,1);
+          end 
       end
       
+      if alfaType == 2 % if bisection
+         alfa = calcAlfa(X, Z, Yd, A, B, gradA, gradB, N); 
+      else
+         alfa = alfa0;
+      end
+      
+      deltaA = alfa * gradA' * [ones(N,1) X];
+      deltaB = alfa * gradB' * [ones(N,1) Z];
+
+      A = A - deltaA;
+      B = B - deltaB;
+
+      
+      %Learning rate update
+      if alfaType == 0
+          alfa = alfa0;
+      elseif alfaType == 1 && mod(it,5) == 0 && alfa > minAlfa
+          alfa = .5 * alfa;
+      end
       
       %Stores some values for plotting
       MSEtrain = [MSEtrain;mse];
       MSEval = [MSEval;mseVal];
+      alfas = [alfas alfa];  
       
-      %{
+      epochsExecuted = it;
+      
       fig=figure(1);
       clf(fig);
       plot(MSEtrain, 'b');
@@ -187,7 +185,7 @@ function [A, B, MSEtrain, MSEval, epochsExecuted] = MLPtreina(X, Yd, Xval, Ydval
       xlabel('Epocas');
       ylabel('Erro Quadratico Medio');            
       legend('train', 'validation');
-      %}
+      
       %{
       alfas = [alfas;alfa];
       fig = figure(2);
@@ -200,4 +198,62 @@ function [A, B, MSEtrain, MSEval, epochsExecuted] = MLPtreina(X, Yd, Xval, Ydval
 
     %close(figure(1));
     %close(figure(2));
+end
+
+function alfa_m = calcAlfa(X, Z, Yd, A, B, gradA, gradB, N)
+
+    gB = gradB' * [ones(N,1) Z];
+    gA = gradA' * [ones(N,1) X];
+    g = [gA(:);gB(:)];
+    d = -g;
+    
+    alfa_l = 0;
+    alfa_u = rand;
+    Aaux = A - alfa_u * gA;
+    Baux = B - alfa_u * gB;
+    g  = calcGrad(X, Yd, Aaux, Baux, N);
+    hl = g'*d;
+
+    while hl<0 
+        alfa_u = alfa_u * 2;
+        Aaux = A - alfa_u * gA;
+        Baux = B - alfa_u * gB;
+        g = calcGrad(X, Yd, Aaux, Baux, N);
+        hl = g'*d;
+    end
+    
+    epsilon = 1e-8;
+    alfa_m = alfa_u;
+    maxIt = ceil(log(alfa_u/epsilon));
+    
+    it = 0;
+    while abs(hl)>epsilon && it < maxIt
+    
+        it = it + 1;
+        alfa_m = (alfa_l+alfa_u)/2;
+        Aaux = A - alfa_m * gA;
+        Baux = B - alfa_m * gB;
+        g = calcGrad(X, Yd, Aaux, Baux, N);
+        hl = g'*d;
+
+        if hl<0
+            alfa_l = alfa_m;
+        else 
+            alfa_u = alfa_m;
+        end
+    end
+end
+
+function [g, gradA, gradB] = calcGrad(X, Yd, A, B, N)
+    
+    [Y, Z] = MLPsaida( X, A, B );
+
+    err = Y-Yd;
+
+    gradB = 1/N * err.*((1-Y).*Y);
+    gradA = 1/N * (gradB*B(:,2:end)).*(Z.*(1-Z));
+    
+    gB = gradB' * [ones(N,1) Z];
+    gA = gradA' * [ones(N,1) X];
+    g = [gA(:);gB(:)];
 end
