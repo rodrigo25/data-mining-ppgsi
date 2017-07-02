3be3ec109ed95e3a655ec655ea542ba65f1321d4
RenanSFreitas <renansouza92@hotmail.com>
2017-07-02 12:16:12 -0300

    remove suporte a mais de 1 camada oculta

--- MLP/MLPsaida.m
+++ MLP/MLPsaida.m
@@ -1,20 +1,7 @@
-function [ Y ] = MLPsaida( X, L, A, B )
+function [ Y, Z ] = MLPsaida( X, A, B )
     N = size(X,1);    
-    if (L==1) && (iscell(A) == 0)
-       X = [ones(N,1),X];
-       Zin = X*A';
-       Z = 1./(1+exp(-Zin));
-       Yin = [ones(N,1),Z]*B';
-       Y = 1./(1+exp(-Yin));
-       return;
-    end
-    
-    for i=1:L
-       X = [ones(N,1),X];
-       Zin{i} = X*A{i}';
-       Z{i} = 1./(1+exp(-Zin{i}));
-       X = Z{i};
-    end
-    Yin = [ones(N,1),Z{i}]*B';
+    Zin = [ones(N,1) X]*A';
+    Z = 1./(1+exp(-Zin));
+    Yin = [ones(N,1) Z]*B';
     Y = 1./(1+exp(-Yin));
 end
