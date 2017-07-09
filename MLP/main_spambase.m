505923c4f0541c563fdcd398323f5a80c1d30bb4
RenanSFreitas <renansouza92@hotmail.com>
2017-07-09 15:25:21 -0300

    Codigo do grid spambase

--- MLP/main_spambase.m
+++ MLP/main_spambase.m
@@ -1,220 +1,163 @@
-function [accuracies, epochsExecuted] = main_spambase()
-
-    % preprocessa spambase
-    % 0 = nao sobrescreve se existir
-    fileName = pre_processing('data_spambase',0);
-    load(fileName);
-   
-    %Varia primeiro apenas neuronios
-    K = preProcInfo.k;
-    neurons = [2 5 10 20 50 100];
-    epochs = 100;
+function [] = main_spambase()
+    % executa varias parametrizacoes de uma MLP com gradiente descendente e
+    % método de bisseção
+    % dados intermediarios sao persistidos assim que possivel 
+    % sao criados graficos ao final da execucao
+    % usa 5-fold cross validation
+    foldCount = 5;
+    numExecutions = 10;
+    neurons = [5 10 20 50 100];
+    epochs = [100 200 500];
+    saveData = 1;
+    fileNames = pre_processing('data_spambase',0, 'kfold', foldCount,'pca');
+    baseResultFileDir = 'grid\spambase\kfold\';
     
-    resultFileDir = ['grid' '\' 'spambase' '\' 'kfold' '\'];
-    if ~exist(resultFileDir, 'dir')
-        mkdir(resultFileDir);
-        for i=1:length(neurons)
-            h = neurons(i);
-            for k=1:K
-                [Xtr, Ytr, Xtest, Ytest, Xval, Yval] = createDatasets(XtrFolds, YtrFolds, XvalFolds, YvalFolds, k);
-                for t=1:30
-                    [ A, B, ~, ~, ep ] = MLPtreina( Xtr, Ytr, Xval, Yval, h, epochs, .01, 0, 1 ); 
-                    Y = MLPsaida( Xtest, A, B );
-                    Y(Y>=.5) = 1;
-                    Y(Y<.5) = 0;
-                    [ ~, ~, stats] = calc_result( Ytest, Y );
-                    [~,acc] = stats.ACC;
-                    fprintf('h = %d\tk = %d\tt = %d\tacc = %f\tep=%d\n', h, k, t, acc, ep);
-                    save([resultFileDir sprintf('h%d_k%d_t%d', h, k, t)], 'acc', 'ep');
-                end 
+    for fileIndex=1:numel(fileNames)
+        resultFileDir = [baseResultFileDir num2str(fileIndex) '\'];
+        if ~exist(resultFileDir, 'dir')
+            mkdir(resultFileDir);
+        end    
+        data = load(fileNames{1});
+        XtrFolds = data.XtrFolds;
+        YtrFolds = data.YtrFolds;
+        XvalFolds = data.XvalFolds;
+        YvalFolds = data.YvalFolds;
+        preProcInfo = data.preProcInfo;
+        classes = preProcInfo.classes;
+        for neuronIndex=1:length(neurons)
+            h = neurons(neuronIndex);
+            for epochsIndex=1:length(epochs)
+               maxEpochs = epochs(epochsIndex);
+               trainMLP(h, maxEpochs, fileIndex, numExecutions, resultFileDir, XtrFolds, YtrFolds, XvalFolds, YvalFolds, classes, foldCount); 
             end
-        end
+        end       
     end
-
-    plotsDir = [resultFileDir '\graficos\'];
+    
+    % plots
+    plotsDir = [baseResultFileDir '\graficos\'];
     if ~exist(plotsDir, 'dir')
         mkdir(plotsDir);
     end
-
-    accuracies = zeros(length(neurons),1);
-    epochsExecuted = zeros(length(neurons),1);
-    for i=1:length(neurons)
-        h = neurons(i);
-        totalAcc = 0;
-        totalEp = 0;
-        for k=1:K
-            for t=1:30
-                load([resultFileDir sprintf('h%d_k%d_t%d', h, k, t)]);
-                totalAcc = totalAcc + acc;
-                totalEp = totalEp + ep;
-            end 
+    for j=1:length(epochs)
+        maxEpochs = epochs(j);
+        accuracies = cell(length(neurons),1);
+        epochsExecuted = cell(length(neurons),1);
+        for i=1:length(neurons)
+            h = neurons(i);
+            accs = [];
+            eps = [];
+            for fileIndex=1:numel(fileNames)
+                for t=1:numExecutions
+                    for k=1:foldCount
+                        load([baseResultFileDir num2str(fileIndex) '\' sprintf('h%d_maxEpochs%d_file%d_t%d_k%d', h, maxEpochs, fileIndex, t, k)]);
+                        accs = [accs acc];
+                        eps  = [eps ep];
+                    end
+                end 
+            end
+            accuracies{i} = accs;
+            epochsExecuted{i} = eps;
         end
-        totalAcc = totalAcc / 30;
-        totalAcc = totalAcc / K;
-
-        totalEp = totalEp / 30;
-        totalEp = totalEp / K;
-        accuracies(i) = totalAcc;
-        epochsExecuted(i) = totalEp;
-    end
-    neuronsAccuracyImpactPlotFile = [plotsDir 'neuronios_acuracia'];
-    if exist(neuronsAccuracyImpactPlotFile, 'file') ~= 2
-        plot(neurons, accuracies);
-        xlabel('Neuronios');
-        ylabel('Acuracia');
-        title('Qtde. de neuronios na camada oculta X Acuracia');
-        print(neuronsAccuracyImpactPlotFile, '-dpng');
-    end
-    
-    %Avaliados os neuronios, tenta aumentar a acuracia dando ao algoritmo
-    %mais epocas de treinamento
-    [~,index] = max(accuracies);
-    maxAccNeurons = neurons(index);
-    epochs = [100 250 500 750 1000];
-    
-    resultFileDir = ['grid' '\' 'spambase' '\' 'kfold' '\' 'epochs' '\'];
-    if ~exist(resultFileDir, 'dir')
-        mkdir(resultFileDir);
-    end
-    for i=1:length(epochs)
-        h = maxAccNeurons;
-        maxEpochs = epochs(i);
-        for k=1:K
-            [Xtr, Ytr, Xtest, Ytest, Xval, Yval] = createDatasets(XtrFolds, YtrFolds, XvalFolds, YvalFolds, k);
-            for t=1:30
-                resultFileName = [resultFileDir sprintf('maxEpochs%d_k%d_t%d', maxEpochs, k, t)];
-                if exist([resultFileName '.mat'],'file') == 2
-                    continue;
-                end
-                [ A, B, ~, ~, ep ] = MLPtreina( Xtr, Ytr, Xval, Yval, h, maxEpochs, .01, 0, 1 ); 
-                Y = MLPsaida( Xtest, A, B );
-                Y(Y>=.5) = 1;
-                Y(Y<.5) = 0;
-                [ ~, ~, stats] = calc_result( Ytest, Y );
-                [~,acc] = stats.ACC;
-                fprintf('h = %d\tk = %d\tt = %d\tacc = %f\tep = %d\tmaxEpochs = %d\n', h, k, t, acc, ep, maxEpochs);
-                save(resultFileName, 'acc', 'ep');
-            end 
-        end
-    end
+        cellAccuracies = accuracies;
+        accuracies = zeros(length(neurons),numel(fileNames)*numExecutions*foldCount);
+        cellEpochsExecuted = epochsExecuted;
+        epochsExecuted = zeros(length(neurons),numel(fileNames)*numExecutions*foldCount);
+        for i=1:length(neurons)
+            accuracies(i,:) = cellAccuracies{i};
+            epochsExecuted(i,:) = cellEpochsExecuted{i};
+        end        
+        meanAccuracies = mean(accuracies,2);
+        stdAccuracies = std(accuracies,1,2);
         
-    accuracies = zeros(length(epochs),1);
-    epochsExecuted = zeros(length(epochs),1);
-    for i=1:length(epochs)
-        h = maxAccNeurons;
-        maxEpochs = epochs(i);
-        totalAcc = 0;
-        totalEp = 0;
-        for k=1:K
-            for t=1:30
-                load([resultFileDir sprintf('maxEpochs%d_k%d_t%d', maxEpochs, k, t)]);
-                totalAcc = totalAcc + acc;
-                totalEp = totalEp + ep;
-            end 
-        end
-        totalAcc = totalAcc / 30;
-        totalAcc = totalAcc / K;
-
-        totalEp = totalEp / 30;
-        totalEp = totalEp / K;
-        accuracies(i) = totalAcc;
-        epochsExecuted(i) = totalEp;
-    end
-    epochsAccuracyImpactPlotFile = [plotsDir 'epocas_acuracia'];
-    if exist(epochsAccuracyImpactPlotFile, 'file') ~= 2
-        plot(epochsExecuted, accuracies, '-X', 'MarkerSize', 12)
-        xlabel('Epocas');
-        ylabel('Acuracia');
-        title('Epocas X Acuracia');
-        print(epochsAccuracyImpactPlotFile, '-dpng');
-    end
-    
-    epochsExecutedMaxEpochsPlotFile = [plotsDir 'epocas_exec_max_epocas'];
-    if exist(epochsExecutedMaxEpochsPlotFile, 'file') ~= 2
-        plot(epochs, epochsExecuted, '--Xr', 'MarkerSize', 12)
-        xlabel('Maximos de epocas');
-        ylabel('Epocas executadas');
-        title('Maxs epocas X Epocas executadas');
-        print(epochsExecutedMaxEpochsPlotFile, '-dpng');
-    end
-    
-    
-    %Avaliados os neuronios e epocas, varia tratamento da taxa de
-    %aprendizado
-    maxAccNeurons = 10;
-    maxEpochs = 500;
-    alfaApproaches = [0 1 2 3];
-    
-    resultFileDir = ['grid' '\' 'spambase' '\' 'kfold' '\' 'alfas' '\'];
-    if ~exist(resultFileDir, 'dir')
-        mkdir(resultFileDir);
-    end
-    for i=1:length(alfaApproaches)
-        h = maxAccNeurons;
-        alfaApproach = alfaApproaches(i);
-        for k=1:K
-            [Xtr, Ytr, Xtest, Ytest, Xval, Yval] = createDatasets(XtrFolds, YtrFolds, XvalFolds, YvalFolds, k);
-            for t=1:30
-                resultFileName = [resultFileDir sprintf('maxEpochs%d_k%d_t%d_alfa%d', maxEpochs, k, t, alfaApproach)];
-                if exist([resultFileName '.mat'],'file') == 2
-                    continue;
-                end
-                
-                if alfaApproach==3
-                    [ A, B ] = treina_rede( Xtr, Ytr, h, maxEpochs );
-                else
-                    [ A, B, ~, ~, ~ ] = MLPtreina( Xtr, Ytr, Xval, Yval, h, maxEpochs, .01, alfaApproach, 1 ); 
-                end
-
-                Y = MLPsaida( Xtest, A, B );
-                Y(Y>=.5) = 1;
-                Y(Y<.5) = 0;
-                [ ~, ~, stats] = calc_result( Ytest, Y );
-                [~,acc] = stats.ACC;
-                fprintf('h = %d\tk = %d\tt = %d\tacc = %f\tep = %d\tmaxEpochs = %d\talfaApproach=%d\n', h, k, t, acc, ep, maxEpochs, alfaApproach);
-                save(resultFileName, 'acc', 'ep');
-            end 
-        end
-    end
+        meanEpochsExecuted = mean(epochsExecuted,2);
         
-    accuracies = zeros(length(alfaApproaches),1);
-    epochsExecuted = zeros(length(alfaApproaches),1);
-    maxEpochs = 500;
-    for i=1:length(alfaApproaches)
-        alfaApproach = alfaApproaches(i);
-        totalAcc = 0;
-        totalEp = 0;
-        for k=1:K
-            for t=1:30
-                load([resultFileDir sprintf('maxEpochs%d_k%d_t%d_alfa%d', maxEpochs, k, t, alfaApproach)]);
-                totalAcc = totalAcc + acc;
-                totalEp = totalEp + ep;
-            end 
+        if ~exist([plotsDir 'neuronios_acuracia'], 'dir')
+            mkdir([plotsDir 'neuronios_acuracia']);
         end
-        totalAcc = totalAcc / 30;
-        totalAcc = totalAcc / K;
+        neuronsAccuracyImpactPlotFile = [plotsDir 'neuronios_acuracia\neuronios_acuracia_' num2str(maxEpochs)];
+        if exist(neuronsAccuracyImpactPlotFile, 'file') ~= 2
+            errorbar(neurons, meanAccuracies, stdAccuracies);
+            xlabel('Neuronios');
+            ylabel('Acuracia');
+            title(sprintf('Qtde. de neuronios na camada oculta X Acuracia (max %d epocas)', maxEpochs));
+            if saveData == 1
+                print(neuronsAccuracyImpactPlotFile, '-dpng');  
+            end
+        end
+        
+        if ~exist([plotsDir 'epocas_acuracia'], 'dir')
+            mkdir([plotsDir 'epocas_acuracia']);
+        end
+        epochsAccuracyImpactPlotFile = [plotsDir 'epocas_acuracia\epocas_acuracia_' num2str(maxEpochs)];
+        if exist(epochsAccuracyImpactPlotFile, 'file') ~= 2
+            plot(meanEpochsExecuted, meanAccuracies, '-X', 'MarkerSize', 12)
+            xlabel('Epocas');
+            ylabel('Acuracia');
+            title('Epocas X Acuracia');
+            if saveData == 1
+                print(epochsAccuracyImpactPlotFile, '-dpng');
+            end
+        end
 
-        totalEp = totalEp / 30;
-        totalEp = totalEp / K;
-        accuracies(i) = totalAcc;
-        epochsExecuted(i) = totalEp;
+        if ~exist([plotsDir 'epocas_exec_neuronios'], 'dir')
+            mkdir([plotsDir 'epocas_exec_neuronios']);
+        end
+        epochsExecutedMaxEpochsPlotFile = [plotsDir 'epocas_exec_neuronios\epocas_exec_neuronios_' num2str(maxEpochs)];
+        if exist(epochsExecutedMaxEpochsPlotFile, 'file') ~= 2
+            plot(neurons, meanEpochsExecuted, '--Xr', 'MarkerSize', 12)
+            xlabel('Neuronios');
+            ylabel('Epocas executadas');
+            title('Neuronios X Epocas executadas');
+            if saveData == 1
+                print(epochsExecutedMaxEpochsPlotFile, '-dpng');
+            end
+        end
     end
 end
 
-function [Xtr, Ytr, Xtest, Ytest, Xval, Yval] = createDatasets(XtrFolds, YtrFolds, XvalFolds, YvalFolds, k)
-    [Xtr, Ytr, Xtest, Ytest] = createFoldDatasets(XtrFolds, YtrFolds, k);
-    [Xval, Yval, ~, ~] = createFoldDatasets(XvalFolds, YvalFolds, k);
-end
+function [] = trainMLP(h, maxEpochs, fileIndex, numExecutions, resultFileDir, XtrFolds, YtrFolds, XvalFolds, YvalFolds, classes, foldCount)
+    %parfor (t=1:numExecutions,3)
+    for k=1:foldCount
+        [Xtr, Ytr, Xtest, Ytest, Xval, Yval] = createDatasets(XtrFolds, YtrFolds, XvalFolds, YvalFolds, k);
+        resultFileNames = cell(numExecutions,1);
+        resultAccs = zeros(numExecutions,1);
+        resultEps = zeros(numExecutions,1);
+        resultYh = cell(numExecutions,1);
+        %tic
+        parfor (t=1:numExecutions,4)
+            resultFileName = [resultFileDir sprintf('h%d_maxEpochs%d_file%d_t%d_k%d', h, maxEpochs, fileIndex, t, k)];
+            resultFileNames{t} = resultFileName;
+            if exist([resultFileName '.mat'], 'file') == 2
+               continue; 
+            end
 
-function [X, Y, Xk, Yk] = createFoldDatasets(XFolds, YFolds, k)
-    
-    Xk = XFolds{k};
-    Yk = YFolds{k}; 
-  
-    X = XFolds;
-    Y = YFolds;
-    X{k} = [];
-    Y{k} = [];
-    X = cell2mat(X(:));
-    Y = cell2mat(Y(:));
+            [ A, B, ~, ~, execEpochs ] = MLPtreina( Xtr, Ytr, Xval, Yval, h, maxEpochs, 2, 0, 1 );
+            %[ A, B, ERROtr, ~, ~ ] = MLP_alfaAdaptativo( Xtr, Ytr, Xval, Yval, h, maxEpochs, 1 );
+            %execEpochs = length(ERROtr);
+
+            Y = MLPsaida( Xtest, A, B );
+            
+            Y(Y >= .5) = 1;
+            Y(Y < .5) = 0;
+
+            [accuracy, ~ ] = multiclassConfusionMatrix( Ytest, Y, classes ); 
+            fprintf('file = %d\tk = %d\tt = %d\tacc = %f\th = %d\tep = %d\n', fileIndex, k, t, accuracy, h, execEpochs);
+            resultAccs(t) = accuracy; 
+            resultEps(t) = execEpochs;
+            resultYh{t} = Y;
+        end 
+        %toc
+        %saves data
+        for t=1:numExecutions
+            resultFileName = resultFileNames{t};
+            if exist([resultFileName '.mat'], 'file') == 2
+               continue; 
+            end
+            acc = resultAccs(t);
+            ep = resultEps(t);
+            Yh = resultYh{t};
+            multiclassConfusionMatrix(Ytest, Yh, classes, 1, 'spambase', [resultFileName '_confusion_matrix'] );
+            save(resultFileName, 'acc', 'ep', 'Yh');
+        end 
+    end
 end
