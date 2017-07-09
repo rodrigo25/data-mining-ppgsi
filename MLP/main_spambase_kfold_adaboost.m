2a947f69aff65a17a08c322b0e90e417f2deb80a
RenanSFreitas <renansouza92@hotmail.com>
2017-07-09 16:59:59 -0300

    spambase adaboost

--- MLP/main_spambase_kfold_adaboost.m
+++ MLP/main_spambase_kfold_adaboost.m
@@ -1,27 +1,65 @@
 function [] = main_spambase_kfold_adaboost()
 
-    fileName = pre_processing('data_spambase',0);
-    load(fileName);
+    neurons = [20];
+    epochs = [1000 5000];
+    T = 35;
+    numExecutions = 1;
+    foldCount = 5;
     
-    K = preProcInfo.k;
-    classes = preProcInfo.classes;
-    
-    T = 5; % adaboost rounds
-    h = 10; % adaboost MLP components hidden layer neuron count
-    maxEpochs = 500; % adaboost MLP components max epoch count
-    
-    Yh = adaboostM2(Xtr, Ytr, Xval, Yval, Xtest, Ytest, classes, T, h, maxEpochs, 0, 0, 0);
-   
-    fprintf('Adaboost global answer (%d components, %d hidden layer neurons, %d epochs)\n', T, h, maxEpochs);
-    [~,Yh]= max(Yh,[],2);
-    [~,Ytest] = max(Ytest,[],2);
-    multiclassConfusionMatrix( Ytest, Yh, classes, 1, 'Adaboost' );
-    
-    % MLP normal para comparar
-    [ A, B ] = MLPtreina( Xtr, Ytr, Xval, Yval, h, maxEpochs, .01, 0, 1);
-    Y = MLPsaida( Xtest, A, B );
-    [~,Y]= max(Y,[],2);
+    fileNames = pre_processing('data_spambase',0, 'kfold', foldCount,'pca');
+    baseResultFileDir = 'ensemble-T35-arch1\spambase\';
+    for fileIndex=1:numel(fileNames)
+        data = load(fileNames{1}); 
+        data = load(fileNames{1});
+        XtrFolds = data.XtrFolds;
+        YtrFolds = data.YtrFolds;
+        XvalFolds = data.XvalFolds;
+        YvalFolds = data.YvalFolds;
+        preProcInfo = data.preProcInfo;
+        classes = preProcInfo.classes;
 
-    fprintf('MLP answer (%d hidden layer neurons, %d epochs)\n', h, maxEpochs);
-    multiclassConfusionMatrix( Ytest, Y, classes, 2, 'MLP' );
+        resultFileDir = [baseResultFileDir num2str(fileIndex) '\'];
+        if ~exist(resultFileDir, 'dir')
+            mkdir(resultFileDir);
+        end
+       
+        for i=1:length(neurons)
+            h = neurons(i);
+            for j=1:length(epochs)
+                maxEpochs = epochs(j);
+                YhEnsemblesPerFold = cell(foldCount,1);
+                parfor (k=1:foldCount,4)
+                    YhEnsembles = cell(numExecutions,1);
+                    [Xtr, Ytr, Xtest, Ytest, Xval, Yval] = createDatasets(XtrFolds, YtrFolds, XvalFolds, YvalFolds, k); 
+                    Ytr = multiclassY(Ytr);
+                    Ytest = multiclassY(Ytest);
+                    Yval = multiclassY(Yval);
+                    for t=1:numExecutions
+                        resultFileName = [resultFileDir sprintf('maxEpochs%d_h%d_t%d_k%d', maxEpochs, h, t, k)];
+                        if exist([resultFileName '.mat'], 'file') == 2
+                           continue; 
+                        end
+                        Yh = adaboostM2(Xtr, Ytr, Xval, Yval, Xtest, Ytest, classes, T, h, maxEpochs, 0);
+                        [~,Yh] = max(Yh,[],2);
+                        YhEnsembles{t} = Yh;
+                        acc = multiclassConfusionMatrix( YtestClasses, Yh, classes );
+                        fprintf('h = %d\tfile = %d\tt = %d\tacc = %f\tmaxEpochs = %d\n', h, fileIndex, t, acc, maxEpochs);
+                    end
+                    YhEnsemblesPerFold{k} = YhEnsembles;
+                end
+                for k=1:foldCount
+                    YhEnsembles = YhEnsemblesPerFold{k}
+                    for t=1:numExecutions
+                        resultFileName = [resultFileDir sprintf('maxEpochs%d_h%d_t%d_k%d', maxEpochs, h, t)];
+                        if exist([resultFileName '.mat'], 'file') == 2
+                           continue; 
+                        end
+                        Yh = YhEnsembles{t};
+                        acc = multiclassConfusionMatrix( YtestClasses, Yh, classes, 1, 'Adaboost', [resultFileName '_confusion_matrix'] );
+                        save(resultFileName, 'acc', 'Yh');
+                    end
+                end
+            end
+        end
+    end
 end
